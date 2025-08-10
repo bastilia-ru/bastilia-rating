bee7cdfe11620ae5c26d42e44baca03898912315
Leonid Tsarev <leo@leotsarev.ru>
2025-08-10 15:34:29 +0300

    форматирование

--- Bastilia.Rating.Domain/BastiliaStatusHistory.cs
+++ Bastilia.Rating.Domain/BastiliaStatusHistory.cs
@@ -1,11 +1,11 @@
-﻿namespace Bastilia.Rating.Domain;
+namespace Bastilia.Rating.Domain;
 
 public record BastiliaStatusHistory(
     DateOnly BeginDate,
     BastiliaStatusType StatusType,
     DateOnly? EndDate)
 {
-    public bool IsActive { get; } = 
-        BeginDate.ToDateTime(TimeOnly.MinValue) < DateTime.Now 
+    public bool IsActive { get; } =
+        BeginDate.ToDateTime(TimeOnly.MinValue) < DateTime.Now
         && (EndDate is null || EndDate.Value.ToDateTime(TimeOnly.MinValue) > DateTime.Now);
 }
