517d48e1d3312637b040c606a4d381bee403f920
Leonid Tsarev <leo@leotsarev.ru>
2025-08-12 13:44:24 +0300

    Добавить сборку мигратора

---
+++ Bastilia.Rating.Migrator/Program.cs
@@ -0,0 +1,20 @@
+using Bastilia.Rating.Database;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Bastilia.Rating.Migrator;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+        builder.Services.RegisterRatingDal(builder.Configuration, builder.Environment);
+        builder.Services.AddHostedService<MigrationsLauncher>();
+        builder.Services.AddScoped<IMigratorService, MigrateEfCoreHostService<AppDbContext>>();
+
+        builder.Build().Run();
+    }
+
+
+}
